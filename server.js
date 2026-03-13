d7ba62905827e884c346c4fea10148f2cdd14416
Mihnea Voiculescu <m.voiculescu@verusflow.onmicrosoft.com>
2026-03-13 23:16:14 +0200

    add basic routing to server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running');
+  } else if (req.url === '/hello') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Mihnea');
+  } else if (req.url === '/status') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
 server.listen(3000, () => {
