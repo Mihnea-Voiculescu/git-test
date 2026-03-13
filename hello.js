32afc29bc0695758940eed9efe084d5512fe6e53
Mihnea Voiculescu <m.voiculescu@verusflow.onmicrosoft.com>
2026-03-13 23:08:12 +0200

    greet function now accepts name parameter

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-function greet() {
-  console.log("Hello from Mihnea");
+function greet(name) {
+  console.log("Hello from " + name);
   console.log("Claude Code is working");
 }
 
-greet();
+greet("Mihnea");
